e501a03fbda72c6ac95be0ec56de64d83bbeb932
Mark Polynice <markpolynice@Alaikas-Air.attlocal.net>
2022-11-17 09:28:58 -0500

    HTML, CSS, Javascript

---
+++ assets/js/script.js
@@ -0,0 +1,98 @@
+const  quizData = [
+    {
+        question: "Which language runs in a web browser?",
+        A: "Java",
+        B: "C",
+        C: "Python",
+        D: "Javascript",
+        Correct: "D",
+    },
+    {
+        question: "What does CSS stand for?",
+        A: "Central Style Sheets",
+        B: "Cascading Style Sheets",
+        C: "Cascading Simple Sheets",
+        D: "Cars SUVs Sailboats",
+        Correct: "B",
+    },
+    {
+        question: "What HTML stand for?",
+        A: "Hypertext Markup Language",
+        B: "Hypertext Markdown Language",
+        C: "Hyperloop Machine Language",
+        D: "Helicopter Terminals Motorboats Lamborginis",
+        Correct: "A",
+
+    },
+    {
+        question: "What year was JavaScript launched?",
+        A: "1996",
+        B: "1995",
+        C: "1994",
+        D: "none of the above",
+        correct: "B",
+    },
+];
+
+const quiz = document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEL = document.getElementById('question')
+const a_text = document.getElementById ('a_text')
+const b_text = document.getElementById ('b_text')
+const c_text = document.getElementById ('c_text')
+const d_text = document.getElementById ('d_text')
+const viewBtn = document.getElementById ('View Highscores')
+const startBtn = document.getElementById ('BEGIN')
+const submitBtn = document.getElementById ('Submit')
+const backBtn = document.getElementById ('Go Back')
+const clearBtn = document.getElementById ('Clear Highscores')
+
+let currentQuiz = 0
+let score = 0
+
+function loadQuiz() {
+
+    deselectAnswers()
+
+    const currentQuizData = quizdata[currentQuiz]
+
+    questionEL.innerText = currentQuizData.question
+    a_text.innertText = currentQuizData.a
+    b_text.innertText = currentQuizData.b
+    c_text.innertText = currentQuizData.c
+    d_text.innertText = currentQuizData.d 
+}
+
+function deselectAnswers() {
+    answerEls.forEach(answerEls => answerEls.checked = false)
+}
+
+function getSelected() {
+    let answerEls
+    answerEls.forEach(answeEl => {
+        if(answerEls.checked) {
+            answer = answerEl.id
+        }
+    })
+    return answer
+}
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++
+        }
+    }
+
+    currentQuiz++
+
+    if(currentQuiz < quizData.length) {
+        loadQuiz()
+    } else {
+        quiz.innerHTML = `
+        <h2> You answered ${score}/ ${quizData.length} questions correctly<h2>
+        
+        <button onclick = "location.reload()"> Reload</button>`
+    } 
+})
